51488d5e0b4416de249c86e26d5c8ff66988dc3b
hectorzaragoza <zaragoza09@gmail.com>
2021-12-07 11:25:54 -0600

    Completed, remember to use  when updating if you don't want to replace your entire document

---
+++ mongo.js
@@ -0,0 +1,45 @@
+{
+    name: 'Han Solo',
+    wantedFor : 'Owing money',
+    client : 'Jabba the Hut',
+    reward : 1000000,
+    ship: 'Millennium Falcon',
+    hunters :['Bobba Fett', 'Dengar', 'IG-88', 'Zuckuss', 'Greedo', 'Bossk', '4-LOM'],
+    captured: false
+  },
+  {
+    name: 'Rocket',
+    wantedFor : 'Stealing Batteries',
+    client : 'Ayesha High Priestess of the Sovereign',
+    reward : 1000000000,
+    ship: 'The Milano',
+    hunters :['Nebula', 'Ravagers'],
+    captured: false
+  },
+  {
+    name: 'Sara Lance',
+    wantedFor : 'Screwing up the timeline, causing anachronisms',
+    client : 'Time Bureau',
+    reward : 50000,
+    ship: 'Waverider',
+    hunters :['Chronos'],
+    captured: false
+  },
+  {
+    name: 'Malcolm Reynolds',
+    wantedFor : 'Aiming to misbehave',
+    client : 'The Alliance',
+    reward : 40000,
+    ship: 'Serenity',
+    hunters :['Jubal Early'],
+    captured: false
+  },
+  {
+    name: 'Starbuck',
+    wantedFor : "Disobeying Captain's orders",
+    client : 'Captain Adama',
+    ship: 'Demetrius',
+    reward : 1000,
+    hunters :['Apollo'],
+    captured: true
+  }
